2c681227e085a46c49fcd29b7bcc734c2a1f3cf7
deniz <denizatlihan2208@gmail.com>
2024-09-02 15:47:18 +0300

    under const

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import { useLanguage } from "../i18n";
+
+const buttonClass =
+  "font-mono font-semibold text-gray-800 hover:text-cyan-700 active:text-cyan-600";
+
+const Header = () => {
+  const { setLanguage } = useLanguage();
+  return (
+    <header className="flex justify-between">
+      <img src="../../ayrota_only.png" className="p-4 h-20"></img>
+      <div className="space-x-2 mr-4 flex flex-row p-4 items-center">
+        <button className={buttonClass} onClick={() => setLanguage("TR")}>
+          TR
+        </button>
+        <div className="font-mono font-semibold">|</div>
+        <button className={buttonClass} onClick={() => setLanguage("EN")}>
+          ENG
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/i18n.tsx
@@ -0,0 +1,65 @@
+import React, { createContext, useState, useContext, ReactNode } from "react";
+
+type Translations = {
+  [key: string]: {
+    [key: string]: string;
+  };
+};
+
+type LanguageContextType = {
+  language: string;
+  setLanguage: (lang: string) => void;
+};
+
+type LanguageProviderProps = {
+  children: ReactNode;
+};
+
+const translations: Translations = {
+  TR: {
+    changeLanguage: "EN",
+    companyName: "Ayrota Yazılım Teknolojileri",
+    insGpsDeniedText:
+      "İHA'lar için GPS Kısıtlı Oramlarda Ataletsel Navigasyon Sistemi Yazılımı",
+    industrialSoftwareText:
+      "Endüstriyel Cihazlar ve Sistemler için Kullanıcı Arayüzü Yazılımları",
+    softwareConsultancy: "Yazılım Danışmanlığı",
+    underConstruction: "Yapım Aşamasında",
+  },
+  EN: {
+    changeLanguage: "TR",
+    companyName: "Ayrota Software Technologies",
+    insGpsDeniedText: "INS Software for UAVs in GPS-Denied Environments",
+    industrialSoftwareText:
+      "Custom Graphical User Interface Softwares for Industrial Devices and Systems",
+    softwareConsultancy: "Software Consultancy",
+    underConstruction: "Under Construction",
+  },
+};
+
+const LanguageContext = createContext<LanguageContextType | undefined>(
+  undefined
+);
+
+export const useLanguage = (): LanguageContextType => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error("language provider error");
+  }
+  return context;
+};
+
+export const LanguageProvider = ({ children }: LanguageProviderProps) => {
+  const [language, setLanguage] = useState<string>("EN");
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const t = (key: string): string => {
+  const { language } = useLanguage();
+  return translations[language][key];
+};
